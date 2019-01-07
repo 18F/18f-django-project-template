aa5a3fed6de26127122806157ca78f0415a9b187
TC Baxter <timothy.baxter@gsa.gov>
2019-01-07 15:00:00 -0600

    Sample home page

--- project_name/context_processors.py
+++ project_name/context_processors.py
@@ -1,4 +1,5 @@
 import datetime
+
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 
--- project_name/urls.py
+++ project_name/urls.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', TemplateView.as_view(template_name="sample_index.html"), name="home"),
 ]
 
 if settings.DEBUG:
